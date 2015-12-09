8726edb829d8722f45f07151cc0a9240bd2330d1
Matt Lambert <mjlambert55@gmail.com>
2015-12-09 21:06:13 +0800

    add home page skeleton

--- public/javascript/controllers/homePageControllers.js
+++ public/javascript/controllers/homePageControllers.js
@@ -8,6 +8,16 @@ var appController = angular.module('homePageControllers', ['ui.bootstrap']);
 appController.controller('homePageController', ['$scope',
     function($scope) {
 
+		$scope.isError = false;
+		$scope.errorMessage = "Unknown Error";
+
+		$scope.gameCode = '';
+
+		$scope.joinGame = function () {
+		};
+
+		$scope.createNewGame = function () {
+		};
 
     }]);
 
