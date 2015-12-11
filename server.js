9b0fdc0fe442aab142cc90bd9d2d4071faea8128
Matt Lambert <mjlambert55@gmail.com>
2015-12-11 23:37:15 +0800

    add sockets! + fixing some errors

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 
 var express     = require('express');
 var app         = express();
+var server      = require('http').createServer(app);
+var io          = require('socket.io')(server);
 var bodyParser  = require('body-parser');
 
 //==============================================================================
@@ -26,8 +28,14 @@ app.use(express.static(__dirname + '/public'));
 require('codeNamesOnline/api/routes')(app);
 
 //==============================================================================
+//  Sockets
+//==============================================================================
+
+require('codeNamesOnline/api/sockets')(io);
+
+//==============================================================================
 //  Start Server
 //==============================================================================
 
-app.listen(8080);
+server.listen(8080);
 console.log("Server started on port 8080");
