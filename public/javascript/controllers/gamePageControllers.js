bb5a68a2ecd54118c2df532b73c59e663d3dd569
Matt Lambert <mjlambert55@gmail.com>
2015-12-13 13:13:05 +0800

    open setup modalon game page

--- public/javascript/controllers/gamePageControllers.js
+++ public/javascript/controllers/gamePageControllers.js
@@ -1,10 +1,40 @@
 (function(){
 'use strict';
 
-var gamePageControllers = angular.module('gamePageControllers', ['ui.bootstrap', 'siteNavigationService']);
+var gamePageControllers = angular.module('gamePageControllers', ['ui.bootstrap', 'siteNavigationService', 'codeNamesAPIService']);
 
-gamePageControllers.controller('gamePageController', ['$scope', '$uibModal', 'siteNavigation',
-	function ($scope, $uibModal, siteNavigation) {
+gamePageControllers.controller('gamePageController', ['$scope', '$uibModal', 'siteNavigation', 'codeNamesAPI',
+	function ($scope, $uibModal, siteNavigation, codeNamesAPI) {
+
+		// Start by opening the setup
+		OpenSetupModal();
+
+		function OpenSetupModal () {
+			var modalInstance = $uibModal.open({
+				templateUrl : '/partials/setup.html',
+				controller  : 'setupController',
+				backdrop    : 'static',
+				keyboard    : false,
+			});
+			modalInstance.result.then(function ModalFinished () {
+			}, function ModalClosed () {
+			});
+		}
+
+	}]);
+
+gamePageControllers.controller('setupController', ['$scope', '$uibModalInstance',
+	function ($scope, $uibModalInstance) {
+
+		$scope.startGame = function () {
+			$uibModalInstance.close();
+		};
+
+
+		// Close modal if user navigates away from page
+		$scope.$on('$routeChangeStart', function () {
+			$uibModalInstance.dismiss();
+		});
 
 	}]);
 
